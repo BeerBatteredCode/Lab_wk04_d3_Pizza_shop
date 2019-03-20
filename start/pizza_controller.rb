331d38be67e60328f9f64d6f8e6f2f63a87dc31b
marisol <mtoscanor37@gmail.com>
2019-03-20 15:26:24 +0000

    lab wk04 day3

---
+++ start/pizza_controller.rb
@@ -0,0 +1,44 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require_relative('./models/pizza_order')
+also_reload('./models/*')
+
+get '/pizza-orders' do #INDEX
+  @orders = PizzaOrder.all()
+  erb( :index )
+end
+
+get '/pizza-orders/new' do #NEW
+  erb( :new )
+end
+
+get '/pizza-orders/:id' do #SHOW
+  @order = PizzaOrder.find(params[:id])
+  erb( :show )
+end
+
+post '/pizza-orders' do #CREATE
+  @order= PizzaOrder.new(params)
+  @order.save
+  erb( :create )
+end
+
+post '/pizza-orders/:id/delete' do #DELETE
+  @order = PizzaOrder.find(params[:id])
+  @order.delete
+  erb( :delete )
+  redirect '/pizza-orders'
+end
+
+get '/pizza-orders/:id/edit' do
+  @order = PizzaOrder.find(params[:id])
+  erb( :edit )
+end
+
+post '/pizza-orders/:id' do
+  @order = PizzaOrder.new(params)
+  p params
+  @order.update
+  erb( :update )
+end
